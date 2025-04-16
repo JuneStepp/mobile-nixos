ac6170181a5d8e7edb3d5bae1d7ff338533a4460
June Stepp <git@junestepp.me>
2025-04-16 12:48:34 -0500

    Use nixpkgs iio-sensor-proxy + latest ssc patches

--- overlay/iio-sensor-proxy-libssc/default.nix
+++ overlay/iio-sensor-proxy-libssc/default.nix
@@ -2,30 +2,41 @@
   iio-sensor-proxy,
   fetchFromGitLab,
   lib,
-  sscSupport ? true,
   libssc,
   libqmi,
   protobufc,
-}:
-
-iio-sensor-proxy.overrideAttrs (old: rec {
-  versoin = "unstable-2024-06-05-ssc";
-  name = "${old.pname}-${versoin}";
-  # https://gitlab.freedesktop.org/hadess/iio-sensor-proxy/-/tree/ssc
-  src = fetchFromGitLab {
-    domain = "gitlab.freedesktop.org";
-    owner = "hadess";
-    repo = old.pname;
-    rev = "49e4f30e95d12e596cf501b994101ad20a23058e";
-    hash = "sha256-OnUPxc/NLgQ5eihzvgFXssFqdRECMIj0J6h0sZMt1DU=";
+}: let
+  pmaports = fetchFromGitLab {
+    domain = "gitlab.postmarketos.org";
+    owner = "postmarketOS";
+    repo = "pmaports";
+    sparseCheckout = ["temp/iio-sensor-proxy"];
+    rev = "f127ff65405a04e1f49ee90c5c980f853c324e89";
+    hash = "sha256-DCJnO8I10ODzFBG21ciZxSaWwYcCFKXMGEUDlFaVczA=";
   };
-  mesonFlags = old.mesonFlags ++ [ (lib.mesonBool "ssc-support" sscSupport) ];
-  # nativeBuildInputs = old.nativeBuildInputs ++ lib.optional sscSupport protobufc;
-  buildInputs =
-    old.buildInputs
-    ++ lib.optionals sscSupport [
-      libssc
-      libqmi
-      protobufc
-    ];
-})
+  # From https://gitlab.postmarketos.org/postmarketOS/pmaports/-/blob/master/temp/iio-sensor-proxy/APKBUILD
+  patchNames = [
+    "0001-iio-sensor-proxy-depend-on-libssc.patch"
+    "0002-proximity-support-SSC-proximity-sensor.patch"
+    "0003-light-support-SSC-light-sensor.patch"
+    "0004-accelerometer-support-SSC-accelerometer-sensor.patch"
+    "0005-compass-support-SSC-compass-sensor.patch"
+    "0006-data-add-libssc-udev-rules.patch"
+    "0007-data-iio-sensor-proxy.service.in-add-AF_QIPCRTR.patch"
+    "0008-drv-ssc-implement-set_polling.patch"
+    "0009-tests-integration-test-add-SSC-sensors.patch"
+  ];
+in
+  iio-sensor-proxy.overrideAttrs (prevAttrs: {
+    patches =
+      (prevAttrs.patches or [])
+      ++ builtins.map (patchName: "${pmaports}/temp/iio-sensor-proxy/${patchName}") patchNames;
+    buildInputs =
+      prevAttrs.buildInputs
+      ++ [
+        libssc
+        libqmi
+        protobufc
+      ];
+    mesonFlags = prevAttrs.mesonFlags ++ [(lib.mesonBool "ssc-support" true)];
+  })
